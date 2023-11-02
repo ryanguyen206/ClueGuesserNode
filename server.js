264a90d8ee72a14fed7c3586462da755e352eaa5
Calvin-Ho-1 <calvinho0302@gmail.com>
2023-11-01 18:33:19 -0700

    Updated Server.js
    Updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
@@ -8,4 +9,16 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+app.use(bodyParser.json());
+
+app.post('/puzzlecreation', (req, res) => {
+
+  const createdPuzzle = req.body;
+
+  res.status(200).json({
+    message: 'puzzle pushed successfully',
+    data: createdPuzzle
+  });
 });
