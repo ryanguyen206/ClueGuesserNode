83af4a1352a9b0ecc61cd57050f28e2ee3913263
ryanguyen206 <ryanninja595@gmail.com>
2023-11-01 19:22:07 -0700

    Create first route

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 
+const cors = require('cors');
+
+var corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}
+
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
@@ -11,6 +21,7 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 app.post('/puzzlecreation', (req, res) => {
